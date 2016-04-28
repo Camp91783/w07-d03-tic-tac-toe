aee53ccd3a22fc20d216bb749a848292b7efad1b
Peter Camp <Pete.f.Camp@gmail.com>
2016-04-28 09:16:55 -0500

    update

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 window.addEventListener("load", setUpPage);
 
+
+ //function changeTurn() {
+		//var changeTurn = document.getElementById('changeTurn');
+	  //changeTurn.setAttribute("onClick", "changeTurn();");
+//} 	 		
+			//if(changeTurn == 'X'){
+           //changeTurn = 'O';
+      //} else {
+           //changeTurn = 'X';
+      //}   
+ 
+
+
 function setUpPage() {
 		var tiles = document.getElementsByClassName("board-tile");
 		for (var i = 0; i < tiles.length; i++ ){
-			tiles[i].addEventListener("click", changeToX())
+			tiles[i].addEventListener("click", changeToX);
 		}
 }
+
+function setUpPage() {
+		var tiles = document.getElementsByClassName("board-tile");
+		for (var i = 0; i < tiles.length; i++ ){
+			tiles[i].addEventListener("click", changeToO);
+		}
+}
+
   // initialize whatever variables you need and add your event listeners
   // you can (but probably shouldn't) define functions inside this function
 
@@ -14,3 +35,9 @@ function setUpPage() {
 		this.innerHTML = "X";
 	} 
 	
+	function changeToO() {
+		this.innerHTML = "O";
+	} 
+
+
+
